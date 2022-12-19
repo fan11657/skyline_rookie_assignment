b2937ba0f7ded0bc052ef080f3678ef0070acd0a
YuFan <fan11657@hotmail.com>
2022-12-20 04:17:28 +0800

    Add gRPC streaming examples

--- HW3/src/GrpcClient/Program.cs
+++ HW3/src/GrpcClient/Program.cs
@@ -1,4 +1,6 @@
+using Grpc.Core;
 using GrpcClient;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,12 +18,57 @@ app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
-app.MapGet("/greet/{name}", async (Greeter.GreeterClient client, string name) =>
+app.MapGet("/greet/once/{name}", async (Greeter.GreeterClient client, string name) =>
 {
     var reply = await client.SayHelloAsync(new HelloRequest { Name = name });
     return new GreetResponse(reply.Message);
 })
-.WithName("Greet");
+.WithName("GreetOnce");
+
+app.MapGet("/greet/many/times/{name}", async (Greeter.GreeterClient client, string name) =>
+{
+    using var call = client.SayHelloStreamingFromServer(new HelloRequest { Name = name });
+    var sb = new StringBuilder();
+    await foreach (var reply in call.ResponseStream.ReadAllAsync())
+    {
+        sb.Append($"{reply.Message} ");
+    }
+    return new GreetResponse(sb.ToString());
+})
+.WithName("GreetManyTimes");
+
+app.MapGet("/greet/many/names/{namesSplitByComma}", async (Greeter.GreeterClient client, string namesSplitByComma) =>
+{
+    var names = namesSplitByComma.Split(',');
+    using var call = client.SayHelloStreamingFromClient();
+    foreach (var name in names)
+    {
+        await call.RequestStream.WriteAsync(new HelloRequest { Name = name });
+    }
+    await call.RequestStream.CompleteAsync();
+    var reply = await call.ResponseAsync;
+    return new GreetResponse(reply.Message);
+})
+.WithName("GreetManyNames");
+
+app.MapGet("/greet/many/times/many/names/{namesSplitByComma}", async (Greeter.GreeterClient client, string namesSplitByComma) =>
+{
+    var names = namesSplitByComma.Split(',');
+    using var call = client.SayHelloStreamingFromBoth();
+    foreach (var name in names)
+    {
+        await call.RequestStream.WriteAsync(new HelloRequest { Name = name });
+    }
+    await call.RequestStream.CompleteAsync();
+
+    var sb = new StringBuilder();
+    await foreach (var reply in call.ResponseStream.ReadAllAsync())
+    {
+        sb.Append($"{reply.Message} ");
+    }
+    return new GreetResponse(sb.ToString());
+})
+.WithName("GreetManyTimesManyNames");
 
 app.Run();
 
--- HW3/src/GrpcService/Services/GreeterService.cs
+++ HW3/src/GrpcService/Services/GreeterService.cs
@@ -1,22 +1,63 @@
 using Grpc.Core;
 using GrpcService;
+using System.Text;
 
 namespace GrpcService.Services
 {
     public class GreeterService : Greeter.GreeterBase
     {
-        private readonly ILogger<GreeterService> _logger;
-        public GreeterService(ILogger<GreeterService> logger)
+        public override async Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
         {
-            _logger = logger;
+            await Task.CompletedTask;
+            return GetHelloReply(request.Name);
         }
 
-        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        public override async Task SayHelloStreamingFromServer(HelloRequest request, IServerStreamWriter<HelloReply> responseStream, ServerCallContext context)
         {
-            return Task.FromResult(new HelloReply
+            const int times = 10;
+            const int msDelay = 100;
+            for (int i = 0; i < times; i++)
             {
-                Message = "Hello " + request.Name
-            });
+                await responseStream.WriteAsync(GetHelloReply(request.Name));
+                await Task.Delay(msDelay);
+            }
+        }
+
+        public override async Task<HelloReply> SayHelloStreamingFromClient(IAsyncStreamReader<HelloRequest> requestStream, ServerCallContext context)
+        {
+            var names = new StringBuilder();
+            await foreach (var request in requestStream.ReadAllAsync()) 
+            {
+                names.Append($"{request.Name}, ");
+            }
+            names.Remove(names.Length - 2, 2);
+            return GetHelloReply(names.ToString());
+        }
+
+        public override async Task SayHelloStreamingFromBoth(IAsyncStreamReader<HelloRequest> requestStream, IServerStreamWriter<HelloReply> responseStream, ServerCallContext context)
+        {
+            var names = new StringBuilder();
+            await foreach (var request in requestStream.ReadAllAsync())
+            {
+                names.Append($"{request.Name}, ");
+            }
+            names.Remove(names.Length - 2, 2);
+
+            const int times = 10;
+            const int msDelay = 100;
+            for (int i = 0; i < times; i++)
+            {
+                await responseStream.WriteAsync(GetHelloReply(names.ToString()));
+                await Task.Delay(msDelay);
+            }
+        }
+
+        private HelloReply GetHelloReply(string name)
+        {
+            return new HelloReply
+            {
+                Message = $"Hello {name}"
+            };
         }
     }
 }
